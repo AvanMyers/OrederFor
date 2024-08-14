e51441b8c6ee4edb80a4b7d0bc41f3690f372ddc
AvanMyers <smail1341@gmail.com>
2024-08-14 16:10:19 +0300

    Initial commit

---
+++ Oreder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oreder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int staticNumber = 7;
+            int starterNumber = 5;
+            string enteredMesage = "";
+            int lastNumber = 103;
+
+            Console.WriteLine($"Счет будет вестись до {lastNumber}");
+
+            for (int i = starterNumber; i <= lastNumber; i += staticNumber)
+            {
+                enteredMesage += Convert.ToString(i) + " ";
+            }
+
+            Console.WriteLine(enteredMesage);
+            Console.ReadKey();
+        }
+    }
+}
